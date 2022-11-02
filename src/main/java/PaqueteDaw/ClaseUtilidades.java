bc2997fce3b6f9c428dc1e7123f8c4c3d6714c9d
Eduardo <eduardocorreaaranda10@gmail.com>
2022-11-02 13:41:23 +0100

    seguir en casa

--- src/main/java/PaqueteDaw/ClaseUtilidades.java
+++ src/main/java/PaqueteDaw/ClaseUtilidades.java
@@ -2,8 +2,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
-
 package PaqueteDaw;
+
+import java.util.InputMismatchException;
+import java.util.Random;
+import java.util.Scanner;
 
 /**
  *
@@ -11,7 +14,57 @@ package PaqueteDaw;
  */
 public class ClaseUtilidades {
 
+    private static Scanner teclado = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int numEntero = 0; 
+        
+        enterosSinErrores();
+        leerEnteroRango();
+        comprobarEnteroRango();
+        generarAleatorioRango();
+    }
+
+    public static void enterosSinErrores() {
+        
+        do {
+            System.out.println("Introduce un numero Entero: ");
+            
+
+            try {
+                numEntero = teclado.nextInt();
+
+                System.out.println("El número es correcto: " + numEntero);
+
+                break;
+            } catch (InputMismatchException ime) {
+
+                System.out.println("No has introducido un número entero,"
+                        + "vuelve a intentarlo");
+
+                teclado.nextLine();
+
+            }
+
+        } while (true);
+    }
+
+    public static void leerEnteroRango() {
+        do{
+         int leerRango;
+         
+         leerRango= teclado.nextInt();
+            
+        }while(true);
+    }
+
+    public static void comprobarEnteroRango() {
+    }
+
+    public static void generarAleatorioRango(int min, int max) {
+        
+        max++;
+        Random numRandom = new Random();
+        int numEntero = numRandom.nextInt(min, max);
     }
 }
